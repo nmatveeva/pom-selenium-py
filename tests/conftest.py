c5745da87696024ef56dc4b44e494e55ed7477ab
Natalia Matveeva <ne.matveeva@gmail.com>
2023-08-24 13:45:16 +0300

    added multiple browsers config

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ This module contains shared fixtures
 
 import pytest
 import selenium.webdriver
+import json
 import os
 from datetime import datetime as dt
 
@@ -20,8 +21,40 @@ def pytest_configure(config: pytest.Config) -> None:
 
 
 @pytest.fixture()
-def browser():
-    br = selenium.webdriver.Chrome()
-    br.implicitly_wait(5)
+def config(scope='session'):
+
+    # Read the file
+    with open('config.json') as config_file:
+        config = json.load(config_file)
+
+    # Assert values are acceptable
+    assert config['browser'] in ['Chrome', 'Headless Chrome', 'Firefox']
+    assert isinstance(config['implicit_wait'], int)
+    assert config['implicit_wait'] > 0
+
+    return config
+
+
+@pytest.fixture()
+def browser(config):
+
+    # Init the Webdriver instance
+    if config['browser'] == 'Chrome':
+        br = selenium.webdriver.Chrome()
+    elif config['browser'] == 'Firefox':
+        br = selenium.webdriver.Firefox()
+    elif config['browser'] == 'Headless Chrome':
+        opts = selenium.webdriver.ChromeOptions()
+        opts.add_argument('headless')
+        br = selenium.webdriver.Chrome(options=opts)
+    else:
+        raise Exception(f'Browser "{config["browser"]}" is not supported')
+
+    # Make its calls wait for elements to appear
+    br.implicitly_wait(config['implicit_wait'])
+
+    # Return the WebDriver instance for the setup
     yield br
+
+    # Quit the WebDriver instance for the cleanup
     br.quit()
--- tests/test_search.py
+++ tests/test_search.py
@@ -17,13 +17,13 @@ def test_basic_bing_search(browser):
     # When the user searches for phrase
     search_page.search(PHRASE)
 
-    # Then the search result title contains phrase
-    assert PHRASE in result_page.title()
-
-    # And the search result query is equal to phrase
+    # Then the search result query is equal to phrase
     assert PHRASE == result_page.search_input_value()
 
     # And the search result links pertain to phrase
     titles = result_page.result_link_titles()
     matches = [title for title in titles if PHRASE.lower() in title.lower()]
     assert len(matches) > 0
+
+    # And the search result title contains phrase
+    assert PHRASE in result_page.title()
